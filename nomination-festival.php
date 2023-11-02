e6e92c4dce747313816731cb441cb21945d29ecd
Aurélie Parret <audatika@hotmail.fr>
2023-11-02 15:31:34 +0100

    Koukaki

---
+++ nomination-festival.php
@@ -0,0 +1,4 @@
+<section id="oscars">
+    <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/oscar.png" alt="Oscars 2021">
+</section>
---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('hero-header').classList.add('loading-video');
+});
