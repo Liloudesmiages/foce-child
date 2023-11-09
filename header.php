9d52750158533159f66e241ea68fcb32370d56e9
Aurélie Parret <audatika@hotmail.fr>
2023-11-09 09:19:31 +0100

    MAJ

--- header.php
+++ header.php
@@ -29,8 +29,6 @@
                 <ul>
                     <li><a href="#story">Histoire</a></li>
                     <li><a href="#characters">Personnages</a></li>
-                    <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-                    <li><a href="#place">Lieu</a></li>
                     <li><a href="#studio">Studio Koukaki</a></li>
                 </ul>
             </div>
